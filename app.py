9ff5b687e7459200a2bbb6eccf82ef338af62d00
Harshad Kotwal <harshadkotwal@Harshads-MacBook-Air.local>
2026-02-03 16:29:29 +0530

    added pdf + converters, seo updates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from PIL import Image
+from PyPDF2 import PdfMerger
 import os
 
 app = Flask(__name__)
@@ -42,5 +43,69 @@ def download(filename):
     path = os.path.join(OUTPUT_FOLDER, filename)
     return send_file(path, as_attachment=True)
 
+#pdf merger
+@app.route("/merge-pdf", methods=["GET", "POST"])
+def merge_pdf():
+
+    if request.method == "POST":
+        files = request.files.getlist("pdfs")
+
+        merger = PdfMerger()
+
+        output_filename = "merged.pdf"
+        output_path = os.path.join(OUTPUT_FOLDER, output_filename)
+
+        for file in files:
+            path = os.path.join(UPLOAD_FOLDER, file.filename)
+            file.save(path)
+            merger.append(path)
+        
+        merger.write(output_path)
+        merger.close()
+
+        return render_template("download.hmtl", filename=output_filename)
+    return render_template("merge_pdf.html")
+
+#jpg -> png
+@app.route("/jpg-to-png", methods=["GET", "POST"])
+def jpg_to_png():
+    if request.method == "POST":
+        file = request.files["image"]
+
+        input_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(input_path)
+
+        img = Image.open(input_path)
+
+        output_filename = "converted.png"
+        output_path = os.path.join(OUTPUT_FOLDER, output_filename)
+
+        img.save(output_path)
+
+        return render_template("download.html", filename = output_filename)
+    
+    return render_template("jpg_to_png.html")
+
+#png to jpg
+@app.route("/png-to-jpg", methods=["GET", "POST"])
+def png_to_jpg() :
+    if request.method == "POST":
+        file = request.files["image"]
+
+        input_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(input_path)
+
+        img = Image.open(input_path).convert("RGB")
+
+        output_filename = "converted.jpg"
+        output_path = os.path.join(OUTPUT_FOLDER, output_filename)
+
+        img.save(output_path, quality= 90)
+
+        return render_template("download.html", filename = output_filename)
+    return render_template("png_to_jpg.html")
+
 if __name__ == "__main__":
     app.run()
+
+
